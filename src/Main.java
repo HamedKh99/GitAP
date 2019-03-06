3139d55153354de4ec00a55daaf1b115b086e22c
garjania <a.garjani@gmail.com>
2019-03-06 12:31:24 +0330

    changed sth in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int x = 5;
-        System.out.println(x);
+        int z = 10;
+        System.out.println(x + z);
     }
 }
